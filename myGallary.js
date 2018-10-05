c8a459dbcc6042b044b2f050da7b4912031a7a14
XiaojueGuan <guanalbertjone@gmail.com>
2018-10-05 13:52:15 +0800

    seperate javascript from html and add a useful function addLoadEvent

--- myGallary.js
+++ myGallary.js
@@ -1,10 +1,40 @@
+function addLoadEvent(func){
+    var oldOnLoad = window.onload;
+    if (typeof window.onload != 'function'){
+        window.onload = func;
+    }else{
+        window.onload = function() {
+            oldOnLoad();
+            func();
+        };
+    }
+
+};
+function prepareGallary(){
+    if(!document.getElementById)return false;
+    if(!document.getElementsByClassName)return false;
+    var myGallary = document.getElementById('myGallary');
+    if(!myGallary)return false;
+    // if(!document.getElementById('myGallary'))return false;
+    var links = document.getElementsByTagName('a');
+    for(i=0;i<links.length;i++){
+        links[i].onclick = function() {
+            return showPic(this) ? false : true;
+        };
+    }
+
+};
+
+
 function showPic(whichPic){
     var source = whichPic.getAttribute('href');
     var placeHolder = document.getElementById('placeHolder');
+    if(!placeHolder)return false;
     placeHolder.setAttribute('src',source);
-    var title = whichPic.title;
+    var title = whichPic.title ? whichPic.title : "";
     var description = document.getElementById('description');
     description.firstChild.nodeValue = title;
+    return true;
 
 };
 function countBodyChildren(){
@@ -13,4 +43,4 @@ function countBodyChildren(){
     alert(body.nodeType)
 
 }
-// window.onload = countBodyChildren;
+addLoadEvent(prepareGallary);
